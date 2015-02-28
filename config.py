fe80905df508b69b7e13c2ea75e47c14d61537e4
dukiemar <dukiemarshaw@yahoo.com>
2015-02-28 03:58:47 +0000

    create heroku database

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ import os
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'javanddukes'
 
-
+#SQLALCHEMY_DATABASE_URI='postgresql://action@localhost/action'
 SQLALCHEMY_DATABASE_URI="postgres://vidzrwuyuffrca:QGgNREuw1rdKWvH3JrV1QjiSzY@ec2-107-20-234-127.compute-1.amazonaws.com:5432/d1hnnf2me1878c"
