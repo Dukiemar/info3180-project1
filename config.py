b8791a1a5f5bd75385d4544ccd221d505423a6cf
dukiemar <dukiemarshaw@yahoo.com>
2015-02-28 01:12:54 +0000

    setup heroku

--- config.py
+++ config.py
@@ -3,7 +3,5 @@ import os
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'javanddukes'
 
-basedir = os.path.abspath(os.path.dirname(__file__))
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
-SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+SQLALCHEMY_DATABASE_URI = "postgresql://action@localhost/action"
