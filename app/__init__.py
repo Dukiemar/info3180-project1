d33d809726445dcc142dd7215ee66ae5964063aa
dukiemar <dukiemarshaw@yahoo.com>
2015-02-28 04:38:05 +0000

    create heroku database

--- app/__init__.py
+++ app/__init__.py
@@ -7,8 +7,8 @@ app = Flask(__name__)
 
 #app.config['SQLALCHEMY_DATABASE_URI']= 'postgresql://action@localhost/action'
 #app.config['SECRET_KEY']="javanddukes"
-app.config['SQLALCHEMY_DATABASE_URI']="postgresql://vidzrwuyuffrca:QGgNREuw1rdKWvH3JrV1QjiSzY@ec2-107-20-234-127.compute-1.amazonaws.com:5432/d1hnnf2me1878c"
-app.config.from_object('config')
+app.config['SQLALCHEMY_DATABASE_URI']#="postgresql://vidzrwuyuffrca:QGgNREuw1rdKWvH3JrV1QjiSzY@ec2-107-20-234-127.compute-1.amazonaws.com:5432/d1hnnf2me1878c"
+#app.config.from_object('config')
 db=SQLAlchemy(app)
 
 from app import views, models
--- config.py
+++ config.py
@@ -4,4 +4,4 @@ WTF_CSRF_ENABLED = True
 SECRET_KEY = 'javanddukes'
 
 #SQLALCHEMY_DATABASE_URI='postgresql://action@localhost/action'
-SQLALCHEMY_DATABASE_URI="postgresql://vidzrwuyuffrca:QGgNREuw1rdKWvH3JrV1QjiSzY@ec2-107-20-234-127.compute-1.amazonaws.com:5432/d1hnnf2me1878c"
+SQLALCHEMY_DATABASE_URI="postgres://vidzrwuyuffrca:QGgNREuw1rdKWvH3JrV1QjiSzY@ec2-107-20-234-127.compute-1.amazonaws.com:5432/d1hnnf2me1878c"
