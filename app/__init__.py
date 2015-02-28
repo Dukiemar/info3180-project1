e4c296d6ff67ab97ed5b57bc8dc9818f720ee12e
dukiemar <dukiemarshaw@yahoo.com>
2015-02-28 04:26:18 +0000

    create heroku database

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,13 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
-from config import SQLALCHEMY_DATABASE_URI#to remove
+from config import SQLALCHEMY_DATABASE_URI #to remove
 
 app = Flask(__name__)
 
 
 #app.config['SQLALCHEMY_DATABASE_URI']= 'postgresql://action@localhost/action'
 #app.config['SECRET_KEY']="javanddukes"
-app.config['SQLALCHEMY_DATABASE_URI']="postgres://vidzrwuyuffrca:QGgNREuw1rdKWvH3JrV1QjiSzY@ec2-107-20-234-127.compute-1.amazonaws.com:5432/d1hnnf2me1878c"
+app.config['SQLALCHEMY_DATABASE_URI']="postgresl://vidzrwuyuffrca:QGgNREuw1rdKWvH3JrV1QjiSzY@ec2-107-20-234-127.compute-1.amazonaws.com:5432/d1hnnf2me1878c"
 app.config.from_object('config')
 db=SQLAlchemy(app)
 
